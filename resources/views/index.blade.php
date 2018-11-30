ad2bc0407658bd8c5c61a3f18a243c021494297c
Bolaji Ake <akebolaji1986@gmail.com>
2018-11-30 18:56:19 +0100

    Created landing page design and integrated into framework

---
+++ resources/views/index.blade.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Medicare</title>
+
+    <meta name="author" content="themesflat.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}/stylesheets/bootstrap.css" >
+    <link rel="stylesheet" type="text/css" href="{{url('')}}/stylesheets/shortcodes.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}/stylesheets/style.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}/stylesheets/mine.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}/stylesheets/responsive.css">
+    <link rel="stylesheet" type="text/css" href="{{url('')}}/stylesheets/animate.css">
+    <link href="{{url('')}}/images/favicon.png" rel="shortcut icon">
+</head> 
+<body class="header-sticky">
+    <div class="boxed">
+    	<!-- Header --> 
+    	<header id="header" class="header clearfix"> 
+        	<div class="container">
+                <div class="header-wrap clearfix">
+                    <div id="logo" class="logo">
+                        <a href="index.blade.php" rel="home">
+                            <img src="{{url('')}}/images/logo.png" alt="image">
+                        </a>
+                    </div><!-- /.logo -->            
+                    <div class="nav-wrap">
+                        <div class="btn-menu">
+                            <span></span>
+                        </div><!-- //mobile menu button -->
+                        <nav id="mainnav" class="mainnav">
+                            <ul class="menu"> 
+                                <li><a href="{{url('')}}">Home</a></li>
+
+
+
+                                <li ><a href="">Browse Stores</a>
+
+                                </li> 
+
+                                <li><a href="">About</a></li>
+
+                                </li>
+                                <li class="has-sub"><a href="">SIGN UP</a>
+                                    <ul class="submenu">
+                                        <li><a href="{{url('register?type=pharmacy')}}">Pharmacy</a></li>
+                                        <li><a href="{{url('register?type=patient')}}">Patient</a></li>
+
+                                    </ul><!-- /.submenu -->
+                                </li>
+                                <li><a href="login.html" class="btn btn-primary btn-join btn-create">Login</a></li>
+
+
+
+                            </ul><!-- /.menu -->
+                        </nav><!-- /.mainnav -->    
+                    </div><!-- /.nav-wrap -->
+
+                </div><!-- /.header-inner --> 
+            </div>
+        </header><!-- /.header -->
+
+        <!--banner starts-->
+        <div class="jumbotron welcome text-white text-center">
+            <h1 class="display-5 text-center mt-5 mb-5">Welcome to Medicare</h1>
+          <p class="mt-5 mb-5">
+                <a class="btn btn-primary btn-lg" href="" role="button">Start Searching   <span class="ml-3 fa fa-angle-right"></span></a>
+            </p>
+        </div>
+        <!--banner ends-->
+
+
+
+
+
+
+
+
+        <!-- Bottom -->
+        <div class="bottom" style="margin-top: -67px !important;">
+            <div class="container">
+
+                <div class="row">
+                    <div class="container-bottom">
+                        <div class="copyright"> 
+                            <p>Medicare Copyright © 2018 All Rights Reserved.</p>
+                        </div>
+                    </div><!-- /.container-bottom -->
+                </div><!-- /.row -->
+            </div><!-- /.container -->
+        </div>
+    </div><!-- /. boxed -->
+
+
+        <!-- Javascript -->
+    <script type="text/javascript" src="javascript/jquery.min.js"></script>
+    <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.easing.js"></script> 
+    <script type="text/javascript" src="javascript/owl.carousel.js"></script> 
+    <script type="text/javascript" src="javascript/jquery-waypoints.js"></script>
+    <script type="text/javascript" src="javascript/jquery-countTo.js"></script>    
+    <script type="text/javascript" src="javascript/parallax.js"></script>
+    <script type="text/javascript" src="javascript/jquery.cookie.js"></script>
+    <script type="text/javascript" src="javascript/jquery-validate.js"></script>     
+    <script type="text/javascript" src="javascript/main.js"></script>
+
+</body>
+
+</html>
